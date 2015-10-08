32545e10738dc9dc3a0504bb768a612d3746288f
dante <dante@demaxiya.local>
2015-10-08 14:14:28 +0800

    dierge

---
+++ Demo02-/Demo02-/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo02-
+//
+//  Created by dante on 15/10/8.
+//  Copyright (c) 2015年 dante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo02-/Demo02-/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Demo02-
+//
+//  Created by dante on 15/10/8.
+//  Copyright (c) 2015年 dante. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    ///aaaa
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
